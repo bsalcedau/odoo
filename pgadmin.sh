bf4103c84c88d470f7b3cf4d491931fcf05b6608
bsalcedau <167237822+bsalcedau@users.noreply.github.com>
2024-09-06 19:42:06 +0200

    Update pgadmin.sh

--- pgadmin.sh
+++ pgadmin.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
-#
-# Setup the repository
-#
 
 sudo apt update && sudo apt install curl -y
 
-# Install the public key for the repository (if not done previously):
+#Descarga clave
 curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
 
-# Create the repository configuration file:
+#Añadir clave al repositorio
 sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
 
-#
-# Install pgAdmin
-#
-
-# Install for desktop mode only:
+#Instalacion de pgadmin en modo escritorio
 sudo apt install pgadmin4-desktop -y
 
+#Cambio de la contraseña del usuario postgres
 sudo su - postgres <<EOF
 psql <<EOF2
 alter role postgres password 'abcd';
